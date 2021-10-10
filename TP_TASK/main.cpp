4218323dfd47296de374bd3e5323e40e6d9f8805
Konstantin <derfreimensch@yandex.ru>
2021-10-10 19:34:42 +0300

    Исправлено добавление ассортимента из файла

--- TP_TASK/main.cpp
+++ TP_TASK/main.cpp
@@ -32,14 +32,20 @@ int main() {
 			std::string path;
 			std::cout << "Ok, write a path to txt file" << std::endl;
 			std::cin >> path;
-			if (update_auto(path) != 0) {
-				std::cout << "Some went wrong, check log file" << std::endl;
+			int ret;
+			ret = update_auto(path);
+			if (ret == 1) {
+				std::cout << "Cannot read name, check config file" << std::endl;
 			}
-			else if (update_auto(path) == 0) {
+			else if (ret == 2) {
+				std::cout << "Cannot read price, check config file" << std::endl;
+			}
+			else if (ret == 0) {
 				std::cout << "Done!" << std::endl <<std::endl;
 				for (auto const& stuffs : st) {
 					std::cout << '[' << stuffs.name << ';' << stuffs.price << ']' << std::endl;
 				}
+				std::cout << std::endl;
 			}
 			continue;
 		}
@@ -89,17 +95,24 @@ int update_auto(std::string path) {
 	std::ifstream config;
 	std::string line;
 	config.open(path);
-	while (std::getline(config, line)) {
-		staff new_staff;
-		new_staff.name = line.substr(line.find_first_of('[')+1, line.find_first_of(';')-1);
-		new_staff.price = atoi(line.substr(line.find(';'), line.find_first_of(']')-1).c_str());
-		if (new_staff.name.empty() == true || new_staff.price == 0) {
-			return 1;
+	while (config.is_open()) {
+		std::getline(config, line);
+		if (line.empty()) {
+			config.close();
+			return 0;
 		}
-		st.push_back(new_staff);
-		config.close();
+		staff new_staff;
+		new_staff.name = line.substr(line.find('[')+1, line.find_first_of(';')-1);
+		new_staff.price = atoi(line.substr(line.find(';') + 1, line.find_first_of(']') - 1 - line.find(';')).c_str());
+		if (new_staff.name.empty() == true || new_staff.price == 0) {
+			if (new_staff.name.empty() == true) return 1;
+			else if (new_staff.price == 0) return 2;
+		}
+		else {
+			st.push_back(new_staff);
+		}
 	}
-	return 0;
+
 }
 int save_to_file(std::string path) {
 	std::ofstream config;
@@ -108,5 +121,9 @@ int save_to_file(std::string path) {
 		config << '[' << stuffs.name << ';' << stuffs.price << ']' <<std::endl;
 	}
 	config.close();
+	return 0;
+}
+int shopping() {
+
 	return 0;
 }
